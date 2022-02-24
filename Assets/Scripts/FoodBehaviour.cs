c3878c08b35a36d3191bd8497aea419812e2244a
kadin0522 <klwhite@mail.bradley.edu>
2022-02-24 17:23:37 -0600

    foodpart2

--- Assets/Scripts/FoodBehaviour.cs
+++ Assets/Scripts/FoodBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FoodBehaviour : MonoBehaviour
 {
     private Rigidbody rb;
+
+    public float force = 500;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +26,21 @@ public class FoodBehaviour : MonoBehaviour
             rb.useGravity = true;
         }
 
-        if(collision.gameObject.name == "Animal Mouth")
+       /* if(collision.gameObject.name == "Animal Mouth")
         {
             Destroy(gameObject);
+        }
+
+        */
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Hand"))
+        {
+
+
+            rb.AddForce(Vector3.down * force);
         }
     }
 }
