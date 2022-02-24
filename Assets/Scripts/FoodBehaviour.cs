0865fcac7e7a3dc0837aa07f9ed854ad75cc4a46
kadin0522 <klwhite@mail.bradley.edu>
2022-02-24 12:24:34 -0600

    In progress of adding food
    -in progress of adding food that can be picked off a tree and given to an animal

---
+++ Assets/Scripts/FoodBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Hand"))
+        {
+            rb.useGravity = true;
+        }
+
+        if(collision.gameObject.name == "Animal Mouth")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
