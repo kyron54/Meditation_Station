1595c4f9ea4258365e78d84c0fd602709c0587d9
Kyron Larenz Patterson <bluekeshon79@gmail.com>
2022-04-16 22:58:40 -0500

    Updated Main Menu

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public Animator anim;
+    private AudioSource start;
+
     // Start is called before the first frame update
     void Start()
     {
-        OVRInput.Update();
+        start = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,11 +27,21 @@ public class MenuManager : MonoBehaviour
 
     public void MeditationStart()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(PlayStart());
     }
 
     public void BackToMain()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private IEnumerator PlayStart()
+    {
+        anim.SetBool("Fade", true);
+        start.Play();
+
+        yield return new WaitForSeconds(10);
+
+        SceneManager.LoadScene(1);
     }
 }
